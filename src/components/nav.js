b773349a07fc62050ab94a392e1e53dc7c495881
johnkirtley <kirtleyj16@gmail.com>
2020-02-07 16:35:31 -0800

    Added necessary components, created basic nav, and created relevant Routes/Links

---
+++ src/components/nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Nav = () => {
+    return (
+        <div className="nav-container">
+            <h2><Link to='/'>John Kirtley</Link></h2>
+            <ul>
+                <li><Link to='/skills'>Skills</Link></li>
+                <li><Link to='/projects'>Projects</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
