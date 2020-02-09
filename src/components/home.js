543aeb23b47f5ef45b969b81d3c972c0fe69ea1f
johnkirtley <kirtleyj16@gmail.com>
2020-02-08 18:25:11 -0800

    Added styling

--- src/components/home.js
+++ src/components/home.js
@@ -12,9 +12,9 @@ const Home = () => {
                 <h3 className="fullstack-dev">Fullstack Web Developer</h3>
             </div>
             <div className="social-icons">
-                <a href="https://linkedin.com/in/johnkirtley16" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} /></a>
-                <a href="https://github.com/johnkirtley" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github']} /></a>
-                <a href="mailto:kirtleyj16@gmail.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faEnvelope} /></a>
+                <a href="https://linkedin.com/in/johnkirtley16" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'linkedin']} className="linkedin" /></a>
+                <a href="https://github.com/johnkirtley" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={['fab', 'github']} className="github" /></a>
+                <a href="mailto:kirtleyj16@gmail.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faEnvelope} className="email" /></a>
 
 
             </div>
